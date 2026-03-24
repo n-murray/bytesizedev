cd72a8a1d616e815a74716ff65fbbaab938cdb7a
Nicholas Murray <nicholas.murray@live.com>
2026-03-24 21:37:29 +0000

    Fix the light mode Header text.

--- bytesizeweb/src/app/_components/home.tsx
+++ bytesizeweb/src/app/_components/home.tsx
@@ -2,7 +2,7 @@
 export function Home() {
   return (
     <section className="flex-col md:flex-row flex items-center mt-16 mb-16 md:mb-12">
-      <h1 className="ml-0 text-5xl text-white md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="ml-0 text-5xl text-slate-900 dark:text-white md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         ByteSizeDev.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
